c3d6c454bac7d2c14161de80d383fdc76e3e0c27
Rainapster <raitre.3@gmail.com>
2026-06-16 13:48:18 +0200

    test

--- giulio/src/components/ConfirmedList/ConfirmedList.tsx
+++ giulio/src/components/ConfirmedList/ConfirmedList.tsx
@@ -1,7 +1,7 @@
 import type { ConfirmedListProps } from "./ConfirmedList.model";
 import "./ConfirmedList.css";
 
-const ConfirmedList = ({ confirmedList, onDelete }: ConfirmedListProps) => {
+const ConfirmedList = ({ confirmedList}: ConfirmedListProps) => {
   return (
     <div className="p-5 backgroundList vh-100 text-start overflow-auto">
       <h2 className="mb-4">{confirmedList?.length} Partecipanti</h2>
@@ -9,13 +9,6 @@ const ConfirmedList = ({ confirmedList, onDelete }: ConfirmedListProps) => {
         confirmedList.map((participant) => (
           <div key={participant.id} className="d-flex align-items-center justify-content-between mb-2">
             <span>{participant.name} {participant.surname}</span>
-            {/* <button
-              type="button"
-              className="btn btn-sm btn-danger"
-              onClick={() => onDelete(participant.id)}
-            >
-              Elimina
-            </button> */}
           </div>
         ))
       ) : (
